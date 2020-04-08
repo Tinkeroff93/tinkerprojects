e6af2dff93adba0cb683b0a2d610b8a2d5dfdf9b
TINKER <TINKER@DESKTOP-U8217HT>
2020-04-08 11:31:40 +0400

    1

---
+++ tntk1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tntk1
+{
+    class Program
+    {
+
+        public class Student
+        {
+            public int _age;
+
+        }
+        static void Main(String[] args)
+
+        {
+            Int32 myVar = new Int32();
+            myVar = 256;
+
+            Student student = new Student();
+            student._age = 5;
+
+            Calculate(student);
+            Console.WriteLine(student._age);
+            Console.Read();
+        }
+        static void Calculate(Student student)
+        {
+            student._age += 1;
+
+        }
+    }
+}
